8e8a8c1a12f48cc5c65e8b92431e302cfa831d44
dgnz <zedign@gmail.com>
2025-08-25 13:31:10 -0400

    Adding Pages

--- s/store.js
+++ s/store.js
@@ -31,9 +31,9 @@ thsBlg_img_cdn = "c.zedign.com/s/";
 thsBlg_gasJsnPrx = "AKfycbwu10Uml2V4z_UuV8RhWb2I6JVc0QAylXsh7VsojIHCmvO6Pwc";
 thsBlg_reportProductForm = "1FAIpQLSe59VsY0gwDggmP6Lgp3h1gh9O0l1p6aZjgP74HbD5jQmR-vA";
 thsBlg_menulinks = [
-'<a style="text-align:center; background: #555; padding: 3px 5px; color: white; border-radius: 30px; text-decoration: none;" href="https://store.zedign.com/p/post.html"><b style="font-size:150%;line-height:100%">+</b> <span>POST YOUR PRODUCT</span></a>', 
-'<a href="https://art.zedign.com/">Fine Art</a>', 
-'<a href="https://3d.zedign.com"> &nbsp; 3D &nbsp; </a>'
+	'<a style="text-align:center; background: #555; padding: 3px 5px; color: white; border-radius: 30px; text-decoration: none;" href="https://store.zedign.com/p/post.html"><b style="font-size:150%;line-height:100%">+</b> <span>POST YOUR PRODUCT</span></a>',
+	'<a href="https://art.zedign.com/">Fine Art</a>',
+	'<a href="https://3d.zedign.com"> &nbsp; 3D &nbsp; </a>'
 
 ];
 
@@ -139,6 +139,25 @@ function handleBrokenImages() {
 		}
 	});
 	/// HNDLE BRKN IMGS
+}
+
+function populateSearchBox() {
+
+	$('head').append(`<style> .search-container { display: flex; justify-content: center; align-items: center; padding: 0 10px; } .search-form {justify-content: center; display: flex; align-items: center; background-color: #ffffff; _padding: 20px; _border-radius: 8px; _box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1); width: 100%; max-width: 600px; } .search-input { max-width:60%; flex: 1; padding: 10px; border: 1px solid #ccc; border-radius: 4px; font-size: 16px; } .search-button { padding: 10px 15px; border: none; border-radius: 4px; background-color: royalblue; color: white; font-size: 16px; cursor: pointer; margin-left: 10px; transition: background-color 0.3s; } .search-button:hover { background-color: royalblue; } </style>`);
+
+	var eq = "";
+	var title = "";
+
+	var q = new URLSearchParams(window.location.search).get('q');
+	if (q) {
+		eq = `value ="${q}" `;
+		title = `<p>Showing results for: <i>${q}</i></p>`;
+	}
+
+	var searchBoxHtml = ` <div class="search-container"> <form action="/search" method="get" class="search-form"> <input ${eq} type="text" name="q" placeholder="Search " required class="search-input"> <input type="hidden" name="by-date" value="true"> <button type="submit" class="search-button"> &nbsp; <svg width="16" height="16" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" > <circle cx="40" cy="40" r="30" stroke="white" stroke-width="16" fill="none" /> <line x1="60" y1="60" x2="98" y2="98" stroke="white" stroke-width="16" /> </svg> &nbsp; </button> </form> </div> ${title} `;
+
+	$('#cse_searchresults').html(searchBoxHtml);
+
 }
 
 // -------- AMZ/EPN FUNCS ----------
@@ -421,8 +440,8 @@ $(function() {
 
 	menulinks();
 
-	gCSE(thsBlg_cse, thsBlg_cse_adchannel);
-
+	// gCSE(thsBlg_cse, thsBlg_cse_adchannel);
+	populateSearchBox()
 	// 
 	/// amz url clean
 	// *** CLEAN ALL AMZ API URLS to .com/dp/xxx?tag=yyy ***
@@ -482,7 +501,7 @@ $(function() {
 
 			`);
 	// 
-	allBloggerLabels();
+	allBloggerLabels(); // this may lead to Uncaught SyntaxError: Unexpected token < when done locally, fine on remote
 	// 
 });
 
